54c2cc77f380ebe1e10e6613f53bcaa7784a28f1
teoi89 <teofil.lucaci@yahoo.ro>
2022-12-05 00:42:55 +0200

    Add project files.

---
+++ lab2ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab2ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va afisa factorialul unui numar n, n fiind citit
+                de la tastatura. */
+            
+            Console.WriteLine("Insert a number:");
+            
+            ulong n = ulong.Parse(Console.ReadLine());
+            ulong f = 1;
+
+            while (n > 0)
+            {
+                f = f * n;
+                n = n - 1;
+            }
+            Console.WriteLine("You number is: " + f);
+        }
+    }
+}
